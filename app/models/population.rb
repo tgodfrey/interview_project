3bced43d788a517ac618d195c77cea7918e5828b
Tyler Godfrey <tyler.godfrey@gmail.com>
2020-05-01 15:29:17 -0500

    check if the requested year is greater than last known year

--- app/models/population.rb
+++ app/models/population.rb
@@ -3,12 +3,19 @@ class Population < ApplicationRecord
   def self.min_year
     Population.order(year: :asc).first.year.year
   end
+
+  def self.max_year
+    Population.order(year: :desc).first.year.year
   end
 
   def self.get(year)
     year = year.to_i
 
     return 0 if year < min_year
+
+    if year > max_year
+      return Population.find_by_year(Date.new(max_year)).population
+    end
 
     pop = nil
     until pop
