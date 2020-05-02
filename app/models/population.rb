2ae7178e86323009994d6d9628ed48fdf5ff0c22
Tyler Godfrey <tyler.godfrey@gmail.com>
2020-05-02 15:29:22 -0500

    Challenge #2

--- app/models/population.rb
+++ app/models/population.rb
@@ -1,5 +1,7 @@
 class Population < ApplicationRecord
 
+  YEAR_INCREMENT = 10
+
   def self.min_year
     Population.order(year: :asc).first.year.year
   end
@@ -13,19 +15,29 @@ class Population < ApplicationRecord
 
     return 0 if year < min_year
 
-    if year > max_year
-      return Population.find_by_year(Date.new(max_year)).population
-    end
+    return Population.find_by_year(Date.new(max_year)).population if year > max_year
 
     pop = nil
-    until pop
-      pop = Population.find_by_year(Date.new(year))
-      year = year - 1
+    year_last_digit = year % YEAR_INCREMENT
+
+    # This assumes the data will always be in increments of 10 years.
+    if year_last_digit == 0
+      pop = Population.find_by_year(Date.new(year)).population
+    else
+      pop_1 = Population.find_by_year(Date.new(year - year_last_digit))
+      pop_2 = Population.find_by_year(Date.new(year + (YEAR_INCREMENT - year_last_digit)))
+      num_years = (pop_1.year.year - pop_2.year.year).abs
+
+      pop = pop_1.population + (growth_rate(pop_1.population, pop_2.population, num_years) * year_last_digit).to_i
     end
 
-    return pop.population if pop
+    return pop if pop
 
     nil
+  end
+
+  def self.growth_rate(pop_1, pop_2, num_years)
+    (pop_2 - pop_1) / num_years
   end
 
 end
--- spec/models/population_spec.rb
+++ spec/models/population_spec.rb
@@ -3,24 +3,45 @@ require 'rails_helper'
 RSpec.describe Population, type: :model do
 
   it "should accept a year we know and return the correct population" do
-    expect(Population.get(1900)).to eq(76212168)
-    expect(Population.get(1990)).to eq(248709873)
+    aggregate_failures do
+      expect(Population.get(1900)).to eq(76_212_168)
+      expect(Population.get(1990)).to eq(248_709_873)
+    end
   end
 
   it "should accept a year we don't know and return the previous known population" do
-    expect(Population.get(1902)).to eq(76212168)
-    expect(Population.get(1908)).to eq(76212168)
+    aggregate_failures do
+      expect(Population.get(1902)).to eq(79_415_432)
+      expect(Population.get(1908)).to eq(89_025_224)
+    end
   end
 
   it "should accept a year that is before earliest known and return zero" do
-    expect(Population.get(1800)).to eq(0)
-    expect(Population.get(0)).to eq(0)
-    expect(Population.get(-1000)).to eq(0)
+    aggregate_failures do
+      expect(Population.get(1800)).to eq(0)
+      expect(Population.get(0)).to eq(0)
+      expect(Population.get(-1000)).to eq(0)
+    end
   end
 
   it "should accept a year that is after latest known and return the last known population" do
-    expect(Population.get(2000)).to eq(248709873)
-    expect(Population.get(200000)).to eq(248709873)
+    aggregate_failures do
+      expect(Population.get(2000)).to eq(248_709_873)
+      expect(Population.get(200000)).to eq(248_709_873)
+    end
   end
 
+  context "growth_rate" do
+    it "should calculate positive growth rate if pop_2 is larger than pop_1" do
+      expect(Population.growth_rate(10, 100, 5)).to be > 0
+    end
+
+    it "should calculate negative growth rate if pop_1 is larger than pop_2" do
+      expect(Population.growth_rate(100, 10, 5)).to be < 0
+    end
+
+    it "should calculate the slope of a simple regression between to populations" do
+      expect(Population.growth_rate(10, 100, 5)).to eq(18)
+    end
+  end
 end
